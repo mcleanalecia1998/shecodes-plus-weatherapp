8da668cbd2a849ab324c7d75676af44b741e352a
mcleanalecia1998 <139597906+mcleanalecia1998@users.noreply.github.com>
2023-11-06 20:14:18 -0500

    Completed the search engine JS

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  console.log(searchInput.value);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
