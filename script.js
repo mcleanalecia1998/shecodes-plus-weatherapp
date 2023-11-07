d44d0ad97539655ab4be6782662d074615f75279
mcleanalecia1998 <139597906+mcleanalecia1998@users.noreply.github.com>
2023-11-06 20:28:10 -0500

    Added API

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "0fdf50965521a4dae377bbta1o61b23c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
