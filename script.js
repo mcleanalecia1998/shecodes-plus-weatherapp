901f5582110b6279d4cdaa2f9a181da223cdc2a6
mcleanalecia1998 <139597906+mcleanalecia1998@users.noreply.github.com>
2023-11-08 15:20:50 -0500

    Finished API in the search engine

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+function updateWeatherApp(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "0fdf50965521a4dae377bbta1o61b23c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeatherApp);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
